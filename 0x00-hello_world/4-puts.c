f7bc462e79b806fcb52dd3f16fd51925c9fc4a9a
Nyiko Danny Baloyi <nyikodannybaloyi@gmail.com>
2022-04-07 15:58:37 -0700

    Task 4-Hello,puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
 *main - function begins
 *
@@ -7,7 +8,7 @@
 
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (o);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (O);
 }
 
