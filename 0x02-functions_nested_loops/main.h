2cffb12020d2f94de0a9f61698a9604eac327326
Nyiko Danny Baloyi <nyikodannybaloyi@gmail.com>
2022-04-13 05:00:57 -0700

    my git

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,5 @@
 #infidef MAIN_H_INCLUDE
 #define MAIN_H_INCLUDE
-
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -14,5 +13,4 @@ void times_table(void);
 int add(int a, int b);
 void print_to_98(int n);
 void print_times_table(int n);
-
 #endif
