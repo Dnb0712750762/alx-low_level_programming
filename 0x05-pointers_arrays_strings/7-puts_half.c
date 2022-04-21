6059021ea17ea7feb538f1c4fee91be283b5cfc0
Nyiko Danny Baloyi <nyikodannybaloyi@gmail.com>
2022-04-21 11:03:03 -0700

    my commit

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -26,11 +26,10 @@ _putchar(str[i]);
 }
 else if (len % 2)
 {
-for
-(n = (len -1) / 2; n < len -1; n++)
+for (n = (len -1) / 2; n < len -1; n++)
 {
 _putchar(str[n + 1]);
 }
 }
-_putchar('\n')
+_putchar('\n');
 }
