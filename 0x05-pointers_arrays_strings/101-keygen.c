3f93a993b90c4ed891467c7e60d82e5c690ec803
Nyiko Danny Baloyi <nyikodannybaloyi@gmail.com>
2022-04-21 16:50:47 -0700

    my commit

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -10,25 +10,23 @@
 
 int main(void)
 {
+
 int i, sum, n;
 int pass[100];
-
 sum = 0;
-
 srand(time(NULL));
-
 for (i = 0; i < 100; i++)
 {
 pass[i] = rand() % 78;
-
 sum += (pass[i] + '0');
-_putchar(pass[i] + '0');
+putchar(pass[i] + '0');
 if ((2772 - sum) - '0' < 78)
 {
 n = 2772 - sum - '0';
 sum += n;
 _putchar(n + '0');
 break;
+
 }
 }
 return (0);
