0256255f845b42bd4490946532bc91bfa07c2d7c
Nyiko Danny Baloyi <nyikodannybaloyi@gmail.com>
2022-04-23 05:07:31 -0700

    my commit

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -9,22 +9,18 @@
  */
 
 void reverse_array(int *a, int n)
-int temp, s, e;
-
-s = 0
-e = n - 1;
-/**
- *set value in array in temp
- *then place the last array in
- *the first array then place the
- *value in temp to the last array
- */
-
-while (s < e)
 {
-temp = a[s];
-a[s] = a[e];
-a[e] = temp;
+int i;
+int temp;
+
+i = 0;
+n = n - 1;
+
+while (i < n)
+{
+temp = a[i];
+a[i] = a[n];
+a[n] = temp;
 s++;
 e--;
 }
