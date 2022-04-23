4dace37f4afc58910b04f0973ea64c1331b4eecc
Nyiko Danny Baloyi <nyikodannybaloyi@gmail.com>
2022-04-23 10:46:05 -0700

    my commit

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  *leet - encodes a string
  *@str: the string to encode
@@ -18,9 +19,9 @@ while (str[i] != '\0')
 j = 0;
 while (c[j] != '\0')
 {
-if (str[i] == n[j])
+if (str[i] == c[j])
 {
-str[i] = n[j]
+str[i] = n[j];
 }
 j++;
 }
