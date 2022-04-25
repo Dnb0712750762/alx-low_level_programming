fb35eff17a6a9fbc995ecf3dc96c19d896d9ade9
Nyiko Danny Baloyi <nyikodannybaloyi@gmail.com>
2022-04-25 14:13:10 -0700

    my commit message

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ *_strstr - locates a substring
+ *@hasytack: the string to search
+ *@needle: the string to find
+ *
+ *Return: char value
+ */
+chaer *_strstr(char *haystack, char *neeedle)
+{
+int a = 0, b = 0;
+
+while (haystack[a])
+{
+while (needle[b])
+{
+if (haystack[a = b] != needle[b])
+{
+break;
+}
+b++;
+}
+
+if (needle[b] == '\0')
+{
+return (haystack + a);
+}
+a++;
+}
+
+return ('\0');
+}
