fb7ea4d6b44e2159585058fd9397690bc1f26ea0
Nyiko Danny Baloyi <nyikodannybaloyi@gmail.com>
2022-04-25 14:53:05 -0700

    my commit message

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ *set_string - sets the value of a pointer to char
+ *@s: the value to modify
+ *@to: the value to assign
+ *
+ *Return: nothing
+ */
+void set_string(char **s, char *to)
+{
+*s = to;
+}
+
+100-set_string.c
+
+#include "main.h"
+
+/**
+ *set_string - stes the value of a pointer to char
+ *@: content is to be copied
+ *@to: string
+ *Return: void
+ */
+
+void set_string(char **s, char *to)
+{
+*s = to;
+}
