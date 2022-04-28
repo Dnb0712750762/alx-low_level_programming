93d455a062467d7a7e3b0a22c1e2d25a643a75b4
Nyiko Danny Baloyi <nyikodannybaloyi@gmail.com>
2022-04-28 11:06:22 -0700

    my commit message

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- *_strlen_recursion - returns legth of string
+ *_strlen_recursion - returns length of string
  *@s: string
  *Return: the length of a string
  */
@@ -10,6 +10,8 @@ int _strlen_recursion(char *s)
 {
 if (*s == '\0')
 return (0);
+
 else
+
 return (1 + _strlen_recursion(s + 1));
 }
