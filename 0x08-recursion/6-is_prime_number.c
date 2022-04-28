80a1798a1f91b01eb5faeb0f67483cbd35da0014
Nyiko Danny Baloyi <nyikodannybaloyi@gmail.com>
2022-04-28 11:53:48 -0700

    my commit message

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- *is_prime - detects if an input number is a prime number
+ *_is_prime - detects if an input number is a prime number
  *@n: input number
  *@c: iterator
  *Return: 1 if n is a prime number. 0 if n is not a prime number
@@ -19,5 +19,5 @@ else
 
 return (0);
 }
-return (0 + is_prime(n, c + 1));
+return (0 + _is_prime(n, c + 1));
 }
